f07b3f2fc283db7f1c96341a56e6564fe0d5c115
Ignjat Radojicic <ignjatradojicic@gmail.com>
2026-03-15 13:26:56 +0100

    feature: Add assets, styling, and scroll watcher

---
+++ TaskManagement/src/TaskManagement.Web/wwwroot/js/scroll-watcher.js
@@ -0,0 +1,20 @@
+﻿window.scrollWatcher = {
+    _handler: null,
+    _dotnetRef: null,
+
+    init: function (dotnetRef) {
+        this._dotnetRef = dotnetRef;
+        this._handler = function () {
+            var scrolled = window.scrollY > 80;
+            dotnetRef.invokeMethodAsync('OnScroll', scrolled);
+        };
+        window.addEventListener('scroll', this._handler, { passive: true });
+    },
+
+    destroy: function () {
+        if (this._handler) {
+            window.removeEventListener('scroll', this._handler);
+            this._handler = null;
+        }
+    }
+};
