6c462933037bc5caa6f2167c7a7759044995b19b
Ignjat Radojicic <ignjatradojicic@gmail.com>
2026-07-27 23:50:47 +0200

    fix: contract for update username in dto

--- Plantitask/src/Plantitask.Contracts/Users/UpdateUserProfileDto.cs
+++ Plantitask/src/Plantitask.Contracts/Users/UpdateUserProfileDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,9 @@ namespace Plantitask.Core.DTO.Users
     {
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
+
+        [StringLength(24, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 24 characters!")]
+        [RegularExpression(@"^[a-zA-Z0-9_]+$", ErrorMessage = "Username can only contain letters, numbers, and underscores")]
         public string? UserName { get; set; }
     }
 
